33751e0948ad1d072b7e30bdde83afa3081460c9
Umut Katib <87450041+umutkatib@users.noreply.github.com>
2025-11-22 23:57:41 +0300

    admin layout

---
+++ SignalRWebUI/ViewComponents/LayoutComponents/_AdminLayoutSideBarPartialComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.LayoutComponents
+{
+	public class _AdminLayoutSideBarPartialComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
