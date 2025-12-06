3368203d63b15392b14e19ab7395183783adb91d
Umut Katib <87450041+umutkatib@users.noreply.github.com>
2025-12-06 23:51:37 +0300

    progressbar

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -70,5 +70,17 @@ namespace SignalRApi.Hubs
 			var value16 = _menuTableService.TMenuTableCount();
 			await Clients.All.SendAsync("ReceiveMenuTableCount", value16);
 		}
+
+		public async Task SendProgress()
+		{
+			var value1 = _moneyCaseService.TTotalMoneyCaseAmount();
+			await Clients.All.SendAsync("ReceiveTotalMoneyCaseAmount", value1.ToString("0.00" + "₺"));
+
+			var value2 = _orderService.TActiveOrderCount();
+			await Clients.All.SendAsync("ReceiveActiveOrderCount", value2);
+
+			var value3 = _menuTableService.TMenuTableCount();
+			await Clients.All.SendAsync("ReceiveMenuTableCount", value3);
+		}
 	}
 }
